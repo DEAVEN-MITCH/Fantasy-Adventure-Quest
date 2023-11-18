708876ba2477ed7f04e524c9eebae5c6dc23ee3b
panpanguin <19307110117@fudan.edu.cn>
2023-11-18 16:25:08 +0800

    上传BOSS战素材

--- Assets/Scripts/Utilities/Enums.cs
+++ Assets/Scripts/Utilities/Enums.cs
@@ -2,6 +2,10 @@ public enum NPCState{
     Patrol,Chase,Skill,Halt
 }
 
+public enum BossState{
+    Wait, Charge, Barrage1, Barrage2, Nebula, RockFall, Brilliance
+}
+
 public enum AbnormalStatus{
     Frost
 }
